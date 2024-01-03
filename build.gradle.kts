09eb79f7637f351e8669ae0fadc50b300493de9e
Udo Kohlmeyer <udo@vmware.com>
2024-01-04 09:29:36 +1100

    SDGF-190: Version catalog and gradle kotlin script

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) VMware, Inc. 2023-2024. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+  id("java")
+  id("idea")
+  id("eclipse")
+  id("maven-publish")
+  alias(libs.plugins.ben.manes.versions)
+  alias(libs.plugins.littlerobots.version.catalog.update)
+}
+
+group="com.vmware.gemfire"
+
+allprojects {
+  configurations.all {
+    resolutionStrategy.cacheChangingModulesFor(0, "minutes")
+  }
+}
+
+versionCatalogUpdate {
+  // These options will be set as default for all version catalogs
+  sortByKey = true
+  // Referenced that are pinned are not automatically updated.
+  // They are also not automatically kept however (use keep for that).
+  pin {
+  }
+  keep {
+    keepUnusedVersions = true
+    // keep all libraries that aren't used in the project
+    keepUnusedLibraries = true
+    // keep all plugins that aren't used in the project
+    keepUnusedPlugins = true
+  }
+
+}
+
+tasks.withType<DependencyUpdatesTask> {
+  rejectVersionIf {
+    !isPatch(candidate.version, currentVersion)
+  }
+}
+
+fun isPatch(candidateVersion: String, currentVersion: String): Boolean {
+  val candidateSplit = candidateVersion.split(".")
+  val currentSplit = currentVersion.split(".")
+
+  if (candidateSplit.size == currentSplit.size && currentSplit.size == 3) {
+    if (candidateSplit[0] != currentSplit[0]) {
+      return false
+    }
+    if (candidateSplit[1] != currentSplit[1]) {
+      return false
+    }
+  }
+  return true
+}
