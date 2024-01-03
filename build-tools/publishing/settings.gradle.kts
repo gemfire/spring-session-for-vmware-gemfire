bc9a0dd3426e674f976613e5146fd126914f6832
Udo Kohlmeyer <udo@vmware.com>
2024-01-04 09:29:36 +1100

    SDGF-190: Version catalog and gradle kotlin script

---
+++ build-tools/publishing/settings.gradle.kts
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) VMware, Inc. 2022-2024. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+pluginManagement {
+    repositories.gradlePluginPortal()
+    repositories.google()
+    repositories.maven {
+        url = uri("https://repo.spring.io/plugins-release")
+    }
+}
+
+dependencyResolutionManagement{
+    versionCatalogs {
+        create("publishLibs"){
+            from(files("../../gradle/publish.versions.toml"))
+        }
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
