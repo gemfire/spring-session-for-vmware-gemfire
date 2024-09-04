ea726f87f0de62940820f672ae18b802730fb4c1
Udo Kohlmeyer <udo@vmware.com>
2024-09-04 15:22:31 +0200

    SDGF-294: Adding publishing to Artifactory

--- spring-session-data-gemfire/build.gradle.kts
+++ spring-session-data-gemfire/build.gradle.kts
@@ -21,6 +21,7 @@ plugins {
   alias(libs.plugins.nebula.facet)
   id("gemfire-repo-artifact-publishing")
   id("commercial-repositories")
+  id("gemfire-artifactory")
 }
 
 description = "Spring Session For VMware GemFire"
