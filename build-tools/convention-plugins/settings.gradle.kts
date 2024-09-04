790cd328303ba4fce91a075ef86250275696a17a
Udo Kohlmeyer <kohlmu-pivotal@users.noreply.github.com>
2024-09-04 17:43:03 +0200

    SDGF-294/9.15-2.7-1.1.x (#93)

---
+++ build-tools/convention-plugins/settings.gradle.kts
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2024 Broadcom. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+pluginManagement {
+  repositories.mavenCentral()
+  repositories.gradlePluginPortal()
+  repositories.google()
+  repositories.maven {
+    url = uri("https://repo.spring.io/plugins-release")
+  }
+}
+
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+dependencyResolutionManagement {
+  versionCatalogs {
+    create("libs") {
+      from(files("../../gradle/publish.versions.toml"))
+    }
+  }
+}
