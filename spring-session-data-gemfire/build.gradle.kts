834a0e5bc635cb468a01f1a6707c12865786dd73
Patrick Johnson <patrick.johnson@broadcom.com>
2024-10-15 15:51:40 -0700

    SDGF-315: Replace Spring Test with Spring Data test-framework variant

--- spring-session-data-gemfire/build.gradle.kts
+++ spring-session-data-gemfire/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
 
     testCompileOnly(libs.jakarta.servlet.api)
     testImplementation(libs.multithreadedtc)
-    testImplementation(libs.spring.test.gemfire)
+    testImplementation(variantOf(libs.spring.data.gemfire) { classifier("test-framework")})
     testImplementation(libs.assertj.core)
     testImplementation(libs.junit)
     testImplementation(libs.mockito.core)
@@ -96,8 +96,10 @@ dependencies {
     "integrationTestImplementation"(libs.log4j.over.slf4j)
     "integrationTestImplementation"(libs.findbugs.jsr305)
     "integrationTestImplementation"(libs.spring.shell)
+    "integrationTestImplementation"(libs.mockito.core)
+    "integrationTestImplementation"(libs.multithreadedtc)
     "integrationTestImplementation"("org.springframework:spring-test")
-    "integrationTestImplementation"(libs.spring.test.gemfire)
+    "integrationTestImplementation"(variantOf(libs.spring.data.gemfire) { classifier("test-framework")})
 }
 
 sourceSets {
