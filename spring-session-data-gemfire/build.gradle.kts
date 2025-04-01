93f23ccaa4d0eaf2ee052cd6021f301a2f12be2b
Udo Kohlmeyer <udo@vmware.com>
2025-04-01 11:41:13 +1100

    Fix test framework version for integration tests

--- spring-session-data-gemfire/build.gradle.kts
+++ spring-session-data-gemfire/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
 
   testCompileOnly(libs.jakarta.servlet.api)
   testImplementation(libs.multithreadedtc)
-  testImplementation(variantOf(libs.spring.data.gemfire) { classifier("test-framework")})
+  testImplementation(libs.spring.data.gemfire.test.framework)
   testImplementation(libs.assertj.core)
   testImplementation(libs.junit)
   testImplementation(libs.mockito.core)
@@ -102,7 +102,7 @@ dependencies {
   "integrationTestImplementation"(libs.mockito.core)
   "integrationTestImplementation"(libs.multithreadedtc)
   "integrationTestImplementation"("org.springframework:spring-test")
-  "integrationTestImplementation"(variantOf(libs.spring.data.gemfire) { classifier("test-framework")})
+  "integrationTestImplementation"(libs.spring.data.gemfire.test.framework)
 }
 
 sourceSets {
