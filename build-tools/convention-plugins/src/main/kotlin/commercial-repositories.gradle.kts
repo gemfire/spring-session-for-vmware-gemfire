f7cabf7b8fa76155d04495c2c905baf79d35532b
Udo Kohlmeyer <udo.kohlmeyer@broadcom.com>
2025-10-01 12:33:22 +1000

    Spring Session 3.5 GemFire 10.1 1.x

--- build-tools/convention-plugins/src/main/kotlin/commercial-repositories.gradle.kts
+++ build-tools/convention-plugins/src/main/kotlin/commercial-repositories.gradle.kts
@@ -23,13 +23,7 @@ repositories {
   val jsonString = File(repositoryConfigFilePath).readText(Charsets.UTF_8)
   val repositories = groovy.json.JsonSlurper().parseText(jsonString) as Map<*, *>
   (repositories["repositories"] as List<*>).filterNotNull().map { entry -> entry as Map<*, *> }
-    .filter { entry ->
-      return@filter if (entry["private"]!! as Boolean) {
-        enablePrivateCommercialRepos
-      } else {
-        true
-      }
-    }.forEach { entry ->
+    .forEach { entry ->
       entry.apply {
         maven {
           url = uri(entry["url"]!! as String)
--- spring-session-data-gemfire/build.gradle.kts
+++ spring-session-data-gemfire/build.gradle.kts
@@ -9,113 +9,120 @@ import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.StorageOptions
 import nebula.plugin.responsible.TestFacetDefinition
 import java.io.FileInputStream
-import java.util.LinkedList
+import java.util.*
 
 buildscript {
-    dependencies {
-        classpath("com.google.cloud:google-cloud-storage:2.30.2")
-    }
+  dependencies {
+    classpath("com.google.cloud:google-cloud-storage:2.30.2")
+  }
 }
 
 plugins {
-    id("java-library")
-    id("maven-publish")
-    alias(libs.plugins.lombok)
-    alias(libs.plugins.nebula.facet)
-    id("gemfire-repo-artifact-publishing")
-    id("commercial-repositories")
-    id("gemfire-artifactory")
+  id("java-library")
+  id("maven-publish")
+  alias(libs.plugins.lombok)
+  alias(libs.plugins.nebula.facet)
+  alias(libs.plugins.nebula.facet.integration)
+  id("gemfire-repo-artifact-publishing")
+  id("commercial-repositories")
+  id("gemfire-artifactory")
 }
 
 description = "Spring Session For VMware GemFire"
 
 
 java {
-    withJavadocJar()
-    withSourcesJar()
-    toolchain { languageVersion = JavaLanguageVersion.of(17) }
+  withJavadocJar()
+  withSourcesJar()
+  toolchain { languageVersion = JavaLanguageVersion.of(17) }
 }
 
-repositories {
-    mavenCentral()
-    maven { url = uri("https://repo.spring.io/milestone") }
-}
+val testJarClasspath = configurations.create("testJarClasspath")
 
 tasks.withType<JavaCompile>().configureEach {
-    options.compilerArgs.add("-parameters")
+  options.compilerArgs.add("-parameters")
 }
 
 tasks.named<Javadoc>("javadoc") {
-    title =
-        "Spring Session 3.3 for VMware GemFire ${getGemFireBaseVersion()} Java API Reference"
-    isFailOnError = false
+  title =
+    "Spring Session 3.5 for VMware GemFire ${getGemFireBaseVersion()} Java API Reference"
+  isFailOnError = false
 }
 
 publishingDetails {
-    artifactName.set("spring-session-3.3-gemfire-${getGemFireBaseVersion()}")
-    longName.set("Spring Session VMware GemFire")
-    description.set("Spring Session For VMware GemFire")
+  artifactName.set("spring-session-3.5-gemfire-${getGemFireBaseVersion()}")
+  longName.set("Spring Session VMware GemFire")
+  description.set("Spring Session For VMware GemFire")
 }
 
 facets {
-    create("integrationTest") {
-        (this as TestFacetDefinition).includeInCheckLifecycle = false
-    }
+  named("integTest") {
+    parentSourceSet = "main"
+    (this as TestFacetDefinition).testTaskName = "integrationTest"
+    this.includeInCheckLifecycle = false
+  }
+}
+
+sourceSets{
+  named("integTest"){
+    java.srcDir(file("src/integrationTest/java"))
+    resources.srcDir(file("src/integrationTest/resources"))
+  }
 }
 
 dependencies {
-    implementation(platform(libs.spring.framework.bom))
-    implementation(platform(libs.spring.security.bom))
+  implementation(platform(libs.spring.framework.bom))
+  implementation(platform(libs.spring.security.bom))
 
-    implementation("org.springframework:spring-context-support")
-    implementation("org.springframework:spring-jcl")
+  implementation("org.springframework:spring-context-support")
+  implementation("org.springframework:spring-jcl")
 
-    api(libs.spring.data.gemfire)
-    api(libs.spring.session.core)
+  api(libs.spring.data.gemfire)
+  api(libs.spring.session.core)
 
-    implementation(libs.findbugs.jsr305)
-    implementation("org.springframework.security:spring-security-core")
-    implementation("org.springframework.security:spring-security-web")
-    implementation(libs.spring.tx)
-    implementation(libs.jakarta.annotation.api)
+  implementation(libs.findbugs.jsr305)
+  implementation("org.springframework.security:spring-security-core")
+  implementation("org.springframework.security:spring-security-web")
+  implementation(libs.spring.tx)
+  implementation(libs.jakarta.annotation.api)
 
-    compileOnly(libs.bundles.gemfire.dependencies)
+  compileOnly(libs.bundles.gemfire.dependencies)
 
-    compileOnly(libs.jakarta.servlet.api)
-    runtimeOnly(libs.jakarta.servlet.api)
+  compileOnly(libs.jakarta.servlet.api)
+  runtimeOnly(libs.jakarta.servlet.api)
 
-    testImplementation(libs.bundles.gemfire.dependencies)
+  testImplementation(libs.bundles.gemfire.dependencies)
 
-    testCompileOnly(libs.jakarta.servlet.api)
-    testImplementation(libs.multithreadedtc)
-    testImplementation(libs.spring.data.gemfire.test.framework)
-    testImplementation(libs.assertj.core)
-    testImplementation(libs.junit)
-    testImplementation(libs.mockito.core)
-    testImplementation(libs.logback.classic)
-    testImplementation(libs.log4j.over.slf4j)
-    testImplementation("org.springframework:spring-test")
-    testImplementation("org.springframework:spring-web")
+  testCompileOnly(libs.jakarta.servlet.api)
+  testImplementation(libs.multithreadedtc)
+  testImplementation(libs.spring.data.gemfire.test.framework)
+  testImplementation(libs.assertj.core)
+  testImplementation(libs.junit)
+  testImplementation(libs.mockito.core)
+  testImplementation(libs.logback.classic)
+  testImplementation(libs.log4j.over.slf4j)
+  testImplementation("org.springframework:spring-test")
+  testImplementation("org.springframework:spring-web")
 
-    "integrationTestImplementation"(libs.bundles.gemfire.dependencies)
-    "integrationTestImplementation"(libs.junit)
-    "integrationTestImplementation"(libs.assertj.core)
-    "integrationTestImplementation"(libs.logback.classic)
-    "integrationTestImplementation"(libs.log4j.over.slf4j)
-    "integrationTestImplementation"(libs.findbugs.jsr305)
-    "integrationTestImplementation"(libs.spring.shell)
-    "integrationTestImplementation"(libs.mockito.core)
-    "integrationTestImplementation"(libs.multithreadedtc)
-    "integrationTestImplementation"("org.springframework:spring-test")
-    "integrationTestImplementation"(libs.spring.data.gemfire.test.framework)
-    "integrationTestImplementation"(libs.gemfire.testcontainers)
-}
+  "integTestImplementation"(libs.bundles.gemfire.dependencies)
+  "integTestImplementation"(libs.junit)
+  "integTestImplementation"(libs.assertj.core)
+  "integTestImplementation"(libs.logback.classic)
+  "integTestImplementation"(libs.log4j.over.slf4j)
+  "integTestImplementation"(libs.findbugs.jsr305)
+  "integTestImplementation"(libs.spring.shell)
+  "integTestImplementation"(libs.mockito.core)
+  "integTestImplementation"(libs.multithreadedtc)
+  "integTestImplementation"("org.springframework:spring-test")
+  "integTestImplementation"(libs.spring.data.gemfire.test.framework)
+  "integTestImplementation"(libs.gemfire.testcontainers)
 
-sourceSets {
-    named("integrationTest") {
-        java.srcDir(file("src/integrationTest/java"))
-        resources.srcDir(file("src/integrationTest/resources"))
-    }
+  testJarClasspath(libs.spring.session.core)
+  testJarClasspath(libs.spring.security.bom)
+  testJarClasspath("org.springframework.security:spring-security-core")
+  testJarClasspath(libs.spring.data.gemfire) {
+    exclude(group = "org.bouncycastle", module = "bcprov-jdk18on")
+  }
 }
 
 repositories {
@@ -128,7 +135,7 @@ repositories {
       }
     }
   }
-  val listOrderedRepos= LinkedList<ArtifactRepository>()
+  val listOrderedRepos = LinkedList<ArtifactRepository>()
   val values = project.repositories.asMap.values
   values.forEach { artifactRepository ->
     if (artifactRepository is MavenArtifactRepository) {
@@ -150,7 +157,8 @@ tasks {
     doLast {
       val storage =
         StorageOptions.newBuilder().setProjectId(project.properties["docsGCSProject"].toString()).setCredentials(
-          GoogleCredentials.fromStream(FileInputStream(project.properties["docsGCSProjectCredentials"].toString()))).build().getService()
+          GoogleCredentials.fromStream(FileInputStream(project.properties["docsGCSProjectCredentials"].toString()))
+        ).build().getService()
       val blobId = BlobId.of(
         project.properties["docsGCSBucket"].toString(),
         "${publishingDetails.artifactName.get()}/${project.version}/${named("javadocJar").get().outputs.files.singleFile.name}"
@@ -159,60 +167,58 @@ tasks {
       storage.createFrom(blobInfo, named("javadocJar").get().outputs.files.singleFile.toPath())
     }
   }
-  named<ProcessResources>("processIntegrationTestResources") {
+  named<ProcessResources>("processIntegTestResources") {
     duplicatesStrategy = DuplicatesStrategy.INCLUDE
   }
 }
 
-tasks.register<Jar>("testJar") {
-    from(sourceSets.getByName("integrationTest").output)
-    from(sourceSets.main.get().output)
-    from({ configurations.runtimeClasspath.get().filter { it.isDirectory }.plus(
-            configurations.runtimeClasspath.get().filter { !it.isDirectory }.map { zipTree(it) })})
-    archiveFileName = "testJar.jar"
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-
-tasks.getByName<Test>("integrationTest") {
-    dependsOn("testJar")
-    forkEvery = 1
-    maxParallelForks = 4
-    val springTestGemfireDockerImage: String by project
-    systemProperty("spring.test.gemfire.docker.image", springTestGemfireDockerImage)
-    systemProperty("TEST_JAR_PATH", tasks.getByName<Jar>("testJar").outputs.files.singleFile.absolutePath)
-}
-
 private fun getSpringSessionBaseVersion(): String {
-    return getBaseVersion(property("springSessionVersion").toString())
+  return getBaseVersion(property("springSessionVersion").toString())
 }
 
 private fun getGemFireBaseVersion(): String {
-    return getBaseVersion(property("gemfireVersion").toString())
+  val gemfireVersion: String by project
+  return getBaseVersion(gemfireVersion)
 }
 
 private fun getBaseVersion(version: String): String {
-    val split = version.split(".")
-    if (split.size < 2) {
-        throw RuntimeException("version is malformed")
-    }
-    return "${split[0]}.${split[1]}"
+  val split = version.split(".")
+  if (split.size < 2) {
+    throw RuntimeException("version is malformed")
+  }
+  return "${split[0]}.${split[1]}"
 }
 
-tasks.named<Test>("integrationTest") {
-  forkEvery = 1
-  maxParallelForks = 1
+tasks.register<Jar>("testJar") {
+  from(sourceSets.getByName("integTest").output)
+  from(sourceSets.main.get().output)
+  from(testJarClasspath.map { zipTree(it) })
+  archiveFileName = "testJar.jar"
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
 
-  filter {
-    setIncludePatterns("*IntegrationTests", "*IntegrationTest")
-  }
+tasks.named<Test>("integrationTest", Test::class.java) {
+  dependsOn("testJar")
+  forkEvery = 1
+  maxParallelForks = 4
+  this.outputs.upToDateWhen { _ -> false }
+
+  val springTestGemfireDockerImage: String by project
+  System.err.println("Spring Docker image: $springTestGemfireDockerImage")
+  systemProperty("spring.test.gemfire.docker.image", springTestGemfireDockerImage)
+  systemProperty("TEST_JAR_PATH", tasks.getByName<Jar>("testJar").outputs.files.singleFile.absolutePath)
+  filter { includeTestsMatching("*IntegrationTests") }
+
 }
 
 tasks.named<Test>("test") {
   forkEvery = 1
   maxParallelForks = 1
+  this.outputs.upToDateWhen { _ -> false }
+  filter { excludeTestsMatching("*IntegrationTests") }
+}
 
-  filter {
-    setIncludePatterns("*Tests", "*Test")
-    setExcludePatterns("*IntegrationTests", "*IntegrationTest")
-  }
+tasks.named("build"){
+  dependsOn("integrationTest")
+  dependsOn("test")
 }
